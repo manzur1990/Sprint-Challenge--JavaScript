c1529f18e4221f7f0d596b1b5a302992d90304a0
JM <manzur-1990@hotmail.com>
2020-03-27 16:08:16 -0500

    Completed Stretch and Questions

--- challenges/classes.js
+++ challenges/classes.js
@@ -4,10 +4,10 @@
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
 class CuboidMaker{
-    constructor(cube){
-    this.length = cube.length;
-    this.width = cube.width;
-    this.height = cube.height;
+    constructor(attrs){
+    this.length = attrs.length;
+    this.width = attrs.width;
+    this.height = attrs.height;
     };
 
      volume(){
@@ -16,6 +16,7 @@ class CuboidMaker{
     surfaceArea() {
         return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
       };
+    } //Class CuboidMaker Finishes
 
       const cuboid = new CuboidMaker({
         length: 4,
@@ -23,15 +24,39 @@ class CuboidMaker{
         height: 5
       });
 
-} //Class CuboidMaker Finishes
+      
 
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+//console.log(cuboid.volume()); // 100
+//console.log(cuboid.surfaceArea()); // 130
   
   
-  
-  
-
   
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+
+
+class CubeMaker extends CuboidMaker{
+    constructor(attrs){
+        super(attrs);
+        this.length = attrs.length;
+        this.width = attrs.width;
+        this.height= attrs.height;
+    }
+    volume(){ // Volume of a Cube = LENGTH x WIDTH x HEIGHT
+        return this.length * this.width * this.height;
+      };
+
+      surfaceArea() { //Surface Area of a Cube = 6 * a^2
+        return 6 * (this.length * this.length + this.height * this.height + this.width * this.width)
+      };
+   
+} //Class Extends CubeMaker Ends
+const cube = new CubeMaker({
+    length: 4,
+    width: 5,
+    height: 5
+});
+
+console.log(cube.volume()); // 
+console.log(cube.surfaceArea()); // 
